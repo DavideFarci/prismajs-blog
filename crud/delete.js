860cdcaaaee355f6dc9763af39cdce69328c11ac
DavideFarci <yank92.df@gmail.com>
2023-11-22 17:34:38 +0100

    add function deletePost

--- crud/delete.js
+++ crud/delete.js
@@ -1,2 +1,25 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+function deletePost(postToDelete) {
+  prisma.post
+    .delete({
+      where: {
+        id: postToDelete.id,
+      },
+    })
+    .then((post) => {
+      console.log("Post eliminato con successo: ", post);
+    });
+}
+
+const postToDelete = {
+  id: 5,
+  title: "Quinto Post",
+  slug: "quinto-post",
+  image: "url_dell_immagine",
+  content: "Contenuto del quinto post...",
+  published: true,
+};
+
+deletePost(postToDelete);
