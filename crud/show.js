80fb439d00aff7a8c635e68c3be2e6ee9bc43b74
DavideFarci <yank92.df@gmail.com>
2023-11-22 16:26:20 +0100

    add function findPost

--- crud/show.js
+++ crud/show.js
@@ -1,2 +1,16 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+function findPost(id) {
+  prisma.post
+    .findFirst({
+      where: {
+        id: id,
+      },
+    })
+    .then((post) => {
+      console.log(`Post con id ${id}:`, post);
+    });
+}
+
+findPost(7);
