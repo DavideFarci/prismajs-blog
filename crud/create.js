a8f3da3c924cca7ae220316904511c48d87282ec
DavideFarci <yank92.df@gmail.com>
2023-11-22 16:24:35 +0100

    create array posts, add funct. createPost

--- crud/create.js
+++ crud/create.js
@@ -1,2 +1,32 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const posts = require("../posts");
+
+// prisma.post
+//   .createMany({
+//     data: posts.map((post) => {
+//       return post;
+//     }),
+//   })
+//   .then((post) => {
+//     console.log(post);
+//   });
+
+// funzione creazione di un post
+
+function createPost(title, image, content) {
+  prisma.post
+    .create({
+      data: {
+        title: title,
+        slug: title.toLowerCase().trim().replace(" ", "-"),
+        image: image,
+        content: content,
+      },
+    })
+    .then((post) => {
+      console.log(`Nuovo post creato: `, post);
+    });
+}
+
+createPost("settimo post", "url_img", "un post favoloso");
---
+++ posts.js
@@ -0,0 +1,44 @@
+const posts = [
+  {
+    id: 1,
+    title: "Primo Post",
+    slug: "primo-post",
+    image: "url_dell_immagine",
+    content: "Contenuto del primo post...",
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Secondo Post",
+    slug: "secondo-post",
+    image: "url_dell_immagine",
+    content: "Contenuto del secondo post...",
+    published: false,
+  },
+  {
+    id: 3,
+    title: "Terzo Post",
+    slug: "terzo-post",
+    image: "url_dell_immagine",
+    content: "Contenuto del terzo post...",
+    published: true,
+  },
+  {
+    id: 4,
+    title: "Quarto Post",
+    slug: "quarto-post",
+    image: "url_dell_immagine",
+    content: "Contenuto del quarto post...",
+    published: false,
+  },
+  {
+    id: 5,
+    title: "Quinto Post",
+    slug: "quinto-post",
+    image: "url_dell_immagine",
+    content: "Contenuto del quinto post...",
+    published: true,
+  },
+];
+
+module.exports = posts;
